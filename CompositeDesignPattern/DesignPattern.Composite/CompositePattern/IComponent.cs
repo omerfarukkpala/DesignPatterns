cd542822a474e3e494144dc0d5423565f16595e2
Ömer Faruk PALA <101570820+omerfarukkpala@users.noreply.github.com>
2024-11-06 10:32:23 +0300

    CompositeDesignPattern

---
+++ CompositeDesignPattern/DesignPattern.Composite/CompositePattern/IComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPattern.Composite.CompositePattern
+{
+    public interface IComponent
+    {
+        public  int Id { get; set; }
+        public  string Name { get; set; }
+
+        int TotalCount();
+        string Display();// Sidebarımızı(Kategorilerimizi) oluşturacak olan html kodları tutacak. 
+    }
+}
